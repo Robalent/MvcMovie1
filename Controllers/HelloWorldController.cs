bbbe16f10b88f70bc8601064d0566ef3b8f96729
Robalent <balentrocio@gmail.com>
2025-04-09 09:59:27 -0300

    primer commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie3.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ Reemplazar el método index:
+
+        public IActionResult Index()
+        {
+            ViewData["Titulo"] = "Este es mi titulo desde el controlador";
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ Agregar este método:
+        // Requires using System.Text.Encodings.Web;
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcMovie3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
